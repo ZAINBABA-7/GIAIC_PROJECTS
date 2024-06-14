5b62d2fded2d5d70572557a4c5a74ffedc1f6473
zain <sohailmemon815@gmail.com>
2024-06-14 15:09:59 +0100

    second Project

---
+++ Project01_Number-Guessing_Game/index.ts
@@ -0,0 +1,13 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random()*10+1);
+const answer = await inquirer.prompt([{
+    message: "Enter a Number Between 1-10:",
+    type: "number",
+    name: "UserGuessNumber",
+}]);
+if(randomNumber === answer.UserGuessNumber){
+    console.log(`${randomNumber} Conguratulation! you guess a Right Number:`);
+}else{
+    console.log(`${randomNumber} Sorry! you guess a Wrong Number:`);
+}
