20aff22d7d9ebb9ebff826f64ddbe1a033c5522a
zain <sohailmemon815@gmail.com>
2024-07-17 02:16:04 +0500

    second Project

--- Project01_Number-Guessing_Game/index.ts
+++ Project01_Number-Guessing_Game/index.ts
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 const randomNumber = Math.floor(Math.random()*10+1);
+
 const answer = await inquirer.prompt([{
     message: "Enter a Number Between 1-10:",
     type: "number",
